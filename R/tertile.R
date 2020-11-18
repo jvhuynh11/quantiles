2810b3bce1e1b724715f46675e7d9ca943bae943
jvhuynh11 <71096176+jvhuynh11@users.noreply.github.com>
2020-11-18 11:18:06 -1000

    Completed documentation for function and updated DESCRIPTION file.

--- R/tertile.R
+++ R/tertile.R
@@ -1,3 +1,18 @@
+#' Calculate the 33rd and 66th percentiles of a vector
+#'
+#' @param x Numeric vector
+#'
+#' @param na.rm a logical value indicating whether NA values should be stripped before the computation proceeds.
+#'
+#' @return
+#' A named vector containing the 33rd and 66th percentiles of the input.
+#'
+#' @export
+#'
+#' @examples
+#' quantile(c(1:100))
+#'
+#' @importFrom stats quantile
 tertile <- function(x, na.rm = TRUE) {
   if (is.numeric(x) == FALSE)
     stop("input must be a numeric vector")
